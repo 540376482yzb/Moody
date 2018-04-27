7065bb1da473831359a5a8a85870656857309aa9
Zhou <540376482yzb@gmail.com>
2018-04-27 11:33:30 -0700

    working version

--- store.js
+++ store.js
@@ -1,6 +1,5 @@
 import { createStore, applyMiddleware } from 'redux'
-import { composeWithDevTools } from 'redux-devtools-extension'
 import thunk from 'redux-thunk'
 import playReducer from './reducers/play'
-export const store = createStore(playReducer, composeWithDevTools(applyMiddleware(thunk)))
+export const store = createStore(playReducer, applyMiddleware(thunk))
 export default store
